045ad22d554bb73e19ece425811ea196bbf5bda0
Albaryys <6205@holbertonstudents.com>
2023-06-12 10:50:54 +0200

    add documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -7,6 +7,11 @@ Write an empty class Square that defines a square
 
 class Square:
     """class that defines a square"""
-    
+
     def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
