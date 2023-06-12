7e79753288d2db989dca5696e6c1e53cfa6836d9
Albaryys <6205@holbertonstudents.com>
2023-06-12 10:47:29 +0200

    add task 1 square

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""0-square.py
+
+Write an empty class Square that defines a square
+"""
+
+
+class Square:
+    """class that defines a square"""
+    
+    def __init__(self, size):
+        self.__size = size
