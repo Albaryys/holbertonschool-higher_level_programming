7f2ebb7caebcc690293048d41a67fcdcd0edb37b
Albaryys <6205@holbertonstudents.com>
2023-06-22 12:18:26 +0200

    add indent

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -5,10 +5,10 @@
 class BaseGeometry:
     """This class represents a BaseGeometry."""
     def area(self):
-        raise Exception("area() n'est pas implémentée")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if type(value) is not int:
-            raise TypeError(f"{name} doit être un entier")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} doit être supérieur à 0")
+            raise ValueError("{} must be greater than 0".format(name))
