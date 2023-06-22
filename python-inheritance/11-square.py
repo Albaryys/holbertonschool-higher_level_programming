13ca8e41e7d25f42b4c3a0581dacf5edd7f6583f
Albaryys <6205@holbertonstudents.com>
2023-06-22 12:35:10 +0200

    add task 11 and changed indent

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -6,7 +6,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Write an class Square"""
+    """initialize the square"""
     def __init__(self, size):
         super().integer_validator("size", size)
         super().__init__(size, size)
@@ -14,3 +14,6 @@ class Square(Rectangle):
 
     def area(self):
         return self.__size ** 2
+
+    def __str__(self):
+        return (f"[Square] {self.__size}/{self.__size}")
