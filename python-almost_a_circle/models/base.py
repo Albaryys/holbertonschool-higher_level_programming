da179d4a2f561a37e82e939c5f37ad2c82b3a60f
Albaryys <6205@holbertonstudents.com>
2023-06-27 14:33:23 +0200

    add a JSON function Classmethod to base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -30,3 +30,14 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """writes the JSON string representation of list_objs to a file"""
+        filename = cls.__name__ + ".json"
+        if list_objs is None:
+            list_objs = []
+        dict_list = [obj.to_dictionary() for obj in list_objs]
+        json_string = cls.to_json_string(dict_list)
+        with open(filename, 'w') as file:
+            file.write(json_string)
