7e4a7d3d78ed7c837c6185db09a51f45aff2ef14
Albaryys <6205@holbertonstudents.com>
2023-06-29 11:01:47 +0200

    added indent

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+import unittest
+from models.rectangle import Rectangle
+
+if __name__ == '__main__':
+    unittest.main()
--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+import unittest
+from models.square import Square
+
+if __name__ == '__main__':
+    unittest.main()
