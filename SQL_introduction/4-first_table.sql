aa9e52f4940f12749ec5ee81e1192c3eae4e2bcf
Albaryys <a.tily.pro@gmail.com>
2023-07-24 02:12:11 -0700

    add task 4

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table called first_table in the current database in my SQL server.
+-- Creates the table.
+CREATE TABLE IF NOT EXISTS first_table (
+       id INTEGER,
+       name VARCHAR(256)
+)
